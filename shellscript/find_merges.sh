2f66ed5f545d9c0faa2e0542dd3948e24404a639
Lukas P <luk.puehringer@gmail.com>
2016-08-01 16:45:26 -0400

    Adds command description comment

--- shellscript/find_merges.sh
+++ shellscript/find_merges.sh
@@ -2,5 +2,7 @@
 . commit_utils.sh
 repo=$1
 export GIT_DIR=${repo}/.git
+# Lists merge commit id and parents in reverse chronological order
+# MERGE-COMMIT-ID PARENT PARENT ...
 git rev-list --parents --merges HEAD
 
